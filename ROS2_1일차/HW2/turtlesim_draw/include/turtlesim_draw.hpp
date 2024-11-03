82d564b8ddb75dbec8c5efc67dd78e3368b66e46
WJJJ2004 <dldnjswns7412@gmail.com>
2024-11-03 17:54:18 +0900

    백업 3

---
+++ ROS2_1일차/HW2/turtlesim_draw/include/turtlesim_draw.hpp
@@ -0,0 +1,35 @@
+#ifndef TURTLESIM_DRAW_HPP
+#define TURTLESIM_DRAW_HPP
+
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include <turtlesim/srv/set_pen.hpp>
+#include <turtlesim/srv/teleport_absolute.hpp>
+#include <chrono>
+#include <string>
+
+class TurtlesimDraw : public rclcpp::Node 
+{
+public:
+    TurtlesimDraw();
+
+private:
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    geometry_msgs::msg::Twist msg;
+    int shape_choice_;
+    float size_;
+    std::string color_;
+    int pen_thickness_;
+
+    void init_msg();
+    void set_pen(const std::string& color, int thickness);
+    void teleport_turtle(double x, double y, double theta);
+    void draw_shape();
+    void draw_square(float size);
+    void draw_circle(float radius);
+    void draw_triangle(float size);
+};
+
+#endif // TURTLESIM_DRAW_HPP
+
